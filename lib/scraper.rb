ac5dc531066aa894c7c8e2148464e4acbb35c770
Jason Sevilla <jesevillaescobar@gmail.com>
2021-03-15 12:08:09 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
